1e9169718381e3265ac31f926f2f0094e3ffa49d
Guinslym <guinslym@gmail.com>
2016-11-16 00:43:49 -0500

    add script

--- script.py
+++ script.py
@@ -1,24 +1,23 @@
 import shlex
 import random
 import statistics
-import linecache
-import fnmatch
-# import a file
+import my_other_script
 
-linecache.getline(statistics.__file__, 8)
+def is_odd_or_even(words):
+	return (len(words) % 2 == 0)
 
-statistics.median([1, 2, 3, 4, 4])
-
-
-def into_account(m, age=18):
-    a=4
-    b=2
-    m = m/0
-    return m
-
-import pdb
-pdb.runcall(into_account, 7)
-
+def find_if_odd(words):
+	for i in words:
+		answer = is_odd_or_even(i.get('name'))
+		i.update({'answer':'odd'})
+	return words
 
 if __name__ == "__main__":
-    print('hello')
+    users = [
+    		{'name':'guinsly', 'age':35, 'marks': [89,75,92], 'fav_quotes': "like he said 'hello'!"},
+    		{'name':'Matthew', 'age':29, 'marks': [82,79,85], 'fav_quotes': "like he said 'hello'!"},
+    		{'name':'Thomas', 'age':39, 'marks': [], 'fav_quotes': "like he said 'hello'!"}
+    		]
+    users = find_if_odd(users)
+    print(users)
+    linecache.getline(statistics.__file__, 8)
