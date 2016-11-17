aa80ac847eb81b773664cbac67eb85bd409e02a5
Guinslym <guinslym@gmail.com>
2016-11-17 00:07:44 -0500

    students

--- script.py
+++ script.py
@@ -17,4 +17,24 @@ def into_account(m, age=18):
 
 
 if __name__ == "__main__":
-	into_account(3)
+	students = [
+        {'name': 'Philliple Henry',
+            'courses': [
+            {   'course_name':'MATH101',
+                'student_marks': [95,85,92],
+                'rate_my_teacher': 4.5
+            },
+            'courses': [
+            {   'course_name':'French',
+                'student_marks': [75,65,92],
+                'rate_my_teacher': 4.5
+            }
+        },
+        {'name': 'Philliple Henry',
+            'courses': [
+            {   'course_name':'Biology',
+                'student_marks': [75,65,72],
+                'rate_my_teacher': 4.5
+            }
+        }
+    ]
