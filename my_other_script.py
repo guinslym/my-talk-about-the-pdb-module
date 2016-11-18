adc1bb3cd4ed3a94e38bd5a9f6d94c86df821fb8
Guinslym <guinslym@gmail.com>
2016-11-17 22:56:41 -0500

    script

--- my_other_script.py
+++ my_other_script.py
@@ -1,4 +1,26 @@
 import fnmatch
 
+students_list = [
+    {'name': 'Philliple Henry',
+        'courses': [
+        {   'course_name':'MATH101',
+            'student_marks': [95,85,92],
+            'rate_my_teacher': 5
+        },
+        'courses': [
+        {   'course_name':'French',
+            'student_marks': [75,65,92],
+            'rate_my_teacher': 2
+        }
+    },
+    {'name': 'Philliple Henry',
+        'courses': [
+        {   'course_name':'Biology',
+            'student_marks': [75,65,72],
+            'rate_my_teacher': 4.5
+        }
+    }
+]
+
 if __name__ == "__main__":
     print('hello')
--- script.py
+++ script.py
@@ -1,42 +1,23 @@
 import shlex
 import random
 import statistics
-import my_other_script
+from .my_other_script import student_list
 
-def divide_function(denominator):
+def divide(denominator):
     return 10 / denominator
 
-def calling_the_divide_function(value=4):
-    val = value
-    val = divide_by_zero(value):
+def calling_the_divide_function(value=4, other_val=student):
+    val = []
+    for i in value:
+        a = divide(i)
+    return val
 
 def return_a_range(value=5):
-    a = dict()
-    value = range(9)
+    value = range(9):
+    #reversing the list
     value = value([::-1]
-    return value
 
 if __name__ == "__main__":
-	students = [
-        {'name': 'Philliple Henry',
-            'courses': [
-            {   'course_name':'MATH101',
-                'student_marks': [95,85,92],
-                'rate_my_teacher': 5
-            },
-            'courses': [
-            {   'course_name':'French',
-                'student_marks': [75,65,92],
-                'rate_my_teacher': 2
-            }
-        },
-        {'name': 'Philliple Henry',
-            'courses': [
-            {   'course_name':'Biology',
-                'student_marks': [75,65,72],
-                'rate_my_teacher': 4.5
-            }
-        }
-    ]
 
-'Result'
+
+
