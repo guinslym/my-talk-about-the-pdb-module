787383b2e9ee3f9052579e62aba09588b3e920cf
Guinslym <guinslym@gmail.com>
2016-11-17 23:48:47 -0500

    script

--- my_other_script.py
+++ my_other_script.py
@@ -1,6 +1,6 @@
 import fnmatch
 
-students_list = []
+students_list = {'Name': 'Zara', 'Age': 7, 'Class': 'First', 'sentence':'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos modi, sequi nisi repellat dolorum a odio sit qui, aliquid eveniet possimus tempore similique in inventore molestiae minus consequuntur quaerat neque!'}
 
 if __name__ == "__main__":
     print('hello')
--- script.py
+++ script.py
@@ -2,10 +2,11 @@ import shlex
 import sys
 from my_other_script import students_list
 
-def divide(denominator=2):
+def divide(denominator=2, other_val=students_list):
+    import pdb; pdb.set_trace()
     return 10 / denominator
 
-def calling_the_divide_function(value=4, other_val=students_list):
+def calling_the_divide_function(value=4):
     val = []
     for i in value:
         a = divide(i)
@@ -13,7 +14,7 @@ def calling_the_divide_function(value=4, other_val=students_list):
     return val
 
 def create_a_list_of_numbers(value=5):
-    value = range(9)
+    value = list(range(9))
     #reversing the list
     value = value[::-1]
     calling_the_divide_function(value)
