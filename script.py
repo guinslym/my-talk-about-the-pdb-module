18325f57d6b19bd7c909fca7864bb78556d20ff3
Guinslym <guinslym@gmail.com>
2016-11-17 23:06:47 -0500

    script is done

--- script.py
+++ script.py
@@ -3,22 +3,24 @@ import random
 import statistics
 from my_other_script import students_list
 
-def divide(denominator):
+def divide(denominator=2):
     return 10 / denominator
 
 def calling_the_divide_function(value=4, other_val=students_list):
     val = []
     for i in value:
         a = divide(i)
+        val.append(a)
     return val
 
-def return_a_range(value=5):
+def create_a_list_of_numbers(value=5):
     value = range(9)
     #reversing the list
     value = value[::-1]
+    calling_the_divide_function(value)
 
 if __name__ == "__main__":
-    print('a')
+    create_a_list_of_numbers()
 
 
 
