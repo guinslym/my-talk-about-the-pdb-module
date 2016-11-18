3c4434c360f68e71ad10b23fda6e0d526d926001
Guinslym <guinslym@gmail.com>
2016-11-17 23:00:06 -0500

    empty student list

--- my_other_script.py
+++ my_other_script.py
@@ -1,26 +1,6 @@
 import fnmatch
 
-students_list = [
-    {'name': 'Philliple Henry',
-        'courses': [
-        {   'course_name':'MATH101',
-            'student_marks': [95,85,92],
-            'rate_my_teacher': 5
-        },
-        'courses': [
-        {   'course_name':'French',
-            'student_marks': [75,65,92],
-            'rate_my_teacher': 2
-        }
-    },
-    {'name': 'Philliple Henry',
-        'courses': [
-        {   'course_name':'Biology',
-            'student_marks': [75,65,72],
-            'rate_my_teacher': 4.5
-        }
-    }
-]
+students_list = []
 
 if __name__ == "__main__":
     print('hello')
--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import shlex
 import random
 import statistics
-from .my_other_script import student_list
+from my_other_script import student_list
 
 def divide(denominator):
     return 10 / denominator
@@ -13,11 +13,12 @@ def calling_the_divide_function(value=4, other_val=student):
     return val
 
 def return_a_range(value=5):
-    value = range(9):
+    value = range(9)
     #reversing the list
-    value = value([::-1]
+    value = value[::-1]
 
 if __name__ == "__main__":
+    print('a')
 
 
 
