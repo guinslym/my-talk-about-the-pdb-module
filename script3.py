5f35dc7ad960c1e7459c47c9a6ed5b8d10d11469
Guinslym <guinslym@gmail.com>
2016-11-21 16:42:01 -0500

    status

--- script3.py
+++ script3.py
@@ -1,22 +1,23 @@
 import code
+import shlex
 import runpy
 from my_other_script import students_list
 
+def sentences():
+    a,b,c,d = shlex.split("This is the 'OPAG meetup'")
+    return d.upper()
 
 def divide(denominator=2, other_val=students_list):
     """
     divide 10 by a given number
     """
     num_list = []
-    for value in [3,2,1,1]:
+    for value in [3,2,1]:
         denominator *= value
         answer = 10 / denominator
         num_list.append(answer)
+    my_sentence = sentences()
     return num_list
 
 if __name__ == "__main__":
-    import pdb
-    pdb.runcall(divide)
-
-
-
+    divide()
