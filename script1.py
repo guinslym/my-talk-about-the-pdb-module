11dd25d04a6b1c90c7661efcaa66bf672833a4c3
Guinslym <guinslym@gmail.com>
2016-11-23 18:37:59 -0500

    script

--- script1.py
+++ script1.py
@@ -5,14 +5,15 @@ from my_other_script import students_list
 
 def divide(denominator=2, other_val=students_list):
     """
-    divide 10 by a given number
+    dummy function that divide 10 by a given number
+    return: a list
     """
-    num_list = []
+    list_num = []
     for value in [3,2,1,0]:
         denominator *= value
         answer = 10 / denominator
-        num_list.append(answer)
-    return num_list
+        list_num.append(answer)
+    return list_num
 
 if __name__ == "__main__":
     list_of_numbers = divide()
