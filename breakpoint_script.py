3f9c61e039f6fb7b64e0c5044a338e37652a1227
Guinslym <guinslym@gmail.com>
2016-11-23 19:57:26 -0500

    script

--- breakpoint_script.py
+++ breakpoint_script.py
@@ -4,8 +4,11 @@ import runpy
 from my_other_script import students_list
 
 def sentences():
+    """
+    dummy function that returns the last value of a split sentence
+    """
     value = 'Meetup'
-    a,b,c,d = shlelx.split("This is the 'OPAG meetup'")
+    a,b,c,d = shlex.split("This is the 'OPAG meetup'")
     return d.upper()
 
 def divide(denominator=2, other_val=students_list):
@@ -15,7 +18,7 @@ def divide(denominator=2, other_val=students_list):
     """
     list_num = []
     compteur = 0 
-    for value in range(2000):
+    for value in range(2000, 0, -1):
         denominator *= value
         answer = 10 / denominator
         list_num.append(answer)
