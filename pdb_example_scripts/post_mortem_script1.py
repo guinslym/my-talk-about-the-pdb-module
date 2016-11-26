7cb33750338cc05cd23f3f40dbf47fe562ddcf26
Guinslym <guinslym@gmail.com>
2016-11-26 00:47:13 -0500

    adding a file

--- pdb_example_scripts/post_mortem_script1.py
+++ pdb_example_scripts/post_mortem_script1.py
@@ -1,16 +1,10 @@
-import code
-import runpy
-import shlex
-from ptpdb import set_trace
-from my_other_script import students_list
-
-def divide(denominator=2, other_val=students_list):
+#python post_mortem_script1.py
+def divide(denominator=2):
     """
     dummy function that divide 10 by a given number
     return: a list
     """
     list_num = []
-    set_trace()
     for value in [3,2,1,0]:
         denominator *= value
         answer = 10 / denominator
@@ -18,7 +12,11 @@ def divide(denominator=2, other_val=students_list):
     return list_num
 
 if __name__ == "__main__":
-    list_of_numbers = divide()
+    try:
+        list_of_numbers = divide()
+    except:
+        import pdb; pdb.post_mortem()
+
 
 
 
